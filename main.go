6eef5368ead7c8ce9a2a160432858299da18006e
paoloyx <paoloyx@gmail.com>
2024-06-07 09:44:46 +0200

    feat: add features and fixes (#11)
    * feat: add route with params
    
    * fix: add missing data to endpoint

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+		c.String(200, "Hello, World!!")
+	})
+
+	// Custom route to greet user
+	r.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(200, "Hello, %s!", name)
 	})
 
 	r.Run(":8080") // Avvia il server sulla porta 8080
